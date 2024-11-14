2adfa9c407a1186cbe7f133f9dd573379c8ab67f
hyenem <gpals04291@gmail.com>
2024-11-14 17:24:45 +0900

    Feat : Create UserController And Service For SignUp 회원가입 로직과 관련하여 유저 컨트롤러와 서비스 만들었습니다.

--- healog_server/src/main/java/com/healog/mvc/account/model/dto/User.java
+++ healog_server/src/main/java/com/healog/mvc/account/model/dto/User.java
@@ -10,5 +10,88 @@ public class User {
 	private String password;
 	private String createdAtDatetime;
 	private int gender;
+	private String birthday;
+	private String phonenumber;
+	private int trainerExist;
+	
+	User(){}
+	
+	
+	public User(String id, String name, String email, String password, String createdAtDatetime, int gender,
+			String birthday, String phonenumber, int trainerExist) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.createdAtDatetime = createdAtDatetime;
+		this.gender = gender;
+		this.birthday = birthday;
+		this.phonenumber = phonenumber;
+		this.trainerExist = trainerExist;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCreatedAtDatetime() {
+		return createdAtDatetime;
+	}
+	public void setCreatedAtDatetime(String createdAtDatetime) {
+		this.createdAtDatetime = createdAtDatetime;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public int getTrainerExist() {
+		return trainerExist;
+	}
+	public void setTrainerExist(int trainerExist) {
+		this.trainerExist = trainerExist;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password
+				+ ", createdAtDatetime=" + createdAtDatetime + ", gender=" + gender + ", birthday=" + birthday
+				+ ", phonenumber=" + phonenumber + ", trainerExist=" + trainerExist + "]";
+	}
+	
 	
 }
--- healog_server/src/main/java/com/healog/mvc/account/model/service/UserService.java
+++ healog_server/src/main/java/com/healog/mvc/account/model/service/UserService.java
@@ -1,5 +1,11 @@
 package com.healog.mvc.account.model.service;
 
+import com.healog.mvc.account.model.dto.User;
+
 public interface UserService {
+
+	boolean isValidId(String email);
+
+	boolean signUp(User user);
 
 }
